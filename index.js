66e124229f00c9b3a47bbbb79ff73dc4bf2a68fa
enbot <nimbotelho@gmail.com>
2020-02-04 21:20:00 -0300

    add new bot

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ const Schedule = require('./source/schedule');
 
 client.on('ready', () => {
 
-	const channel = client.channels.find(channel => channel.id === '672551565151830022');
+	const channel = client.channels.find(channel => channel.id === '674401444740202542')
 
 	const schedules = [
-		{ timing: { minute: 0, hour: [12, 14, 16, 18, 20, 22, 00, 02], }, message: 'Eu sou gay' },
-		{ timing: { minute: 34, hour: [12, 14, 16, 18, 20, 22, 00, 02, 21], }, message: 'Eu sou gay 2' },
-		{ timing: { minute: 35, hour: [12, 14, 16, 18, 20, 22, 00, 02, 21], }, message: 'Eu sou gay 2' },
+		{ timing: { minute: 0, hour: [12, 14, 16, 18, 20, 22, 00, 02] }, message: 'evento do reino' },
+		{ timing: { minute: 30, hour: [13, 17, 23], }, message: 'World Boss' },
+		{ timing: { minute: 00, hour: 22, dayOfWeek: [2, 4] }, message: 'gvg' },
+		{ timing: { minute: 30, hour: [17, 23], dayOfWeek: [2, 4, 6] }, message: 'duelo das ruinas' },
+		{ timing: { minute: 30, hour: 21, dayOfWeek: [1, 3, 5, 7], }, message: 'baile da guild' },
 	];
 
-	let instances = [];
+	let instances = []
 
 	for (let s of schedules) {
-		instances.push(new Schedule(s.timing, s.message));
+		instances.push(new Schedule(s.timing, s.message))
 	}
 
 	for (let i of instances) {
-		i.start(channel);
+		i.start(channel)
 	}
 
 	let res = 'connected 💙💙💙💙'
@@ -29,4 +31,5 @@ client.on('ready', () => {
 	console.log(res);
 
 });
+
 client.login(config.token);
