f8b0f1fe73930b04a1eebeb0c8bb88b5bcd6c55f
enbot <nimbotelho@gmail.com>
2020-02-16 17:50:27 -0300

    starting date structure

--- source/resources/events.js
+++ source/resources/events.js
@@ -2,6 +2,28 @@ module.exports = [
 
     { timing: { minute: 0, hour: [12, 14, 16, 18, 20, 22, 00, 02], }, name: 'event 1' },
 
-    { timing: { minute: [55, 57], hour: [12, 14, 16, 18, 20, 21, 22, 00, 02], }, name: 'event 2' },
+    { timing: { minute: [52, 54], hour: [12, 14, 15, 18, 20, 21, 22, 00, 02], }, name: 'event 2' },
 
 ]
+
+const week = {
+    "SUNDAY": 0,
+    "MONDAY": 1,
+    "TUESDAY": 2,
+    "WEDNESDAY": 3,
+    "THURSDAY": 4,
+    "FRIDAY": 5,
+    "SATURDAY": 6,
+}
+
+// const days = {
+
+//     ""
+
+// }
+
+/* run the job at 18:55:30 on Dec. 14 2018*/
+// var date = new Date(2018, 11, 14, 18, 56, 30);
+
+/* run the job at 18:55:30 on Dec. 14 2018*/
+// var date = new Date(YYYY, MM, DD, HH, mm, ss);
