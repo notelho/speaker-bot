2337c9560cab43ba269ebf7c39a87e4fa1b7940d
enbot <nimbotelho@gmail.com>
2020-02-16 23:04:10 -0300

    define events

--- source/models/event.js
+++ source/models/event.js
@@ -1,18 +1,47 @@
+const messages = require('../resources/messages')
 const schedule = require('node-schedule')
+const moment = require('moment')
 
 module.exports = class Event {
 
-    constructor(timing, name) {
-        this._timing = timing
+    constructor(name, time, type, gap) {
+
         this._name = name
+        this._time = time
+        this._type = type
+        this._gap = gap
+
+        this._message = messages[type](name)
+
     }
 
-    get schedule(gap) {
-        throw new Error('get schedule() must be implemented')
+    get schedule() {
+        let hour = this._time.hour
+        let minute = this._time.minute
+        let date = new Date()
+
+        date.setHours(hour, minute, 0, 0)
+
+        if (this._gap !== 0)
+            return new Date(date.getTime() - (1000 * 60 * (this._gap * -1)))
+
+        return date
+    }
+
+    get isAfter() {
+
+        const now = new Date().getTime()
+        const schedule = this.schedule.getTime()
+
+        return now < schedule
+    }
+
+    get gap() {
+        return this._gap
     }
 
     get message() {
-        throw new Error('get message() must be implemented')
+        return this._message
     }
 
     get timing() {
@@ -21,6 +50,10 @@ module.exports = class Event {
 
     get name() {
         return this._name
+    }
+
+    get type() {
+        return this._type
     }
 
 }
