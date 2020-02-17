f624c0c452cab3135a2f9b0ed3ab2d52745cb355
enbot <nimbotelho@gmail.com>
2020-02-16 23:02:38 -0300

    add weeks and define events

--- source/resources/events.js
+++ source/resources/events.js
@@ -1,29 +1,31 @@
-module.exports = [
+const week = require('./week')
 
-    { timing: { minute: 0, hour: [12, 14, 16, 18, 20, 22, 00, 02], }, name: 'event 1' },
+const events = [
 
-    { timing: { minute: [52, 54], hour: [12, 14, 15, 18, 20, 21, 22, 00, 02], }, name: 'event 2' },
+    {
+        name: 'Trilha da revelação',
+        times: [
+            { hour: 23, minute: 00 },
+            { hour: 23, minute: 30 },
+        ],
+        days: [
+            week.SUNDAY,
+            week.MONDAY,
+            week.WEDNESDAY,
+            week.FRIDAY,
+        ]
+    },
+
+    {
+        name: 'Torre celestial',
+        times: [
+            { hour: 12, minute: 00 },
+        ],
+        days: [
+            week.SATURDAY,
+        ]
+    }
 
 ]
 
-const week = {
-    "SUNDAY": 0,
-    "MONDAY": 1,
-    "TUESDAY": 2,
-    "WEDNESDAY": 3,
-    "THURSDAY": 4,
-    "FRIDAY": 5,
-    "SATURDAY": 6,
-}
-
-// const days = {
-
-//     ""
-
-// }
-
-/* run the job at 18:55:30 on Dec. 14 2018*/
-// var date = new Date(2018, 11, 14, 18, 56, 30);
-
-/* run the job at 18:55:30 on Dec. 14 2018*/
-// var date = new Date(YYYY, MM, DD, HH, mm, ss);
+module.exports = events
